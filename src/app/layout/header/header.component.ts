ac1b86f1489e1aa3b956566b26b440f6b5972b45
kevindsousa <kevindiegodasilvasousa@gmail.com>
2023-09-12 09:08:01 -0300

    style: Create menu fixed

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   public logo = "../../assets/logo-white.png"
+
+  isFixed: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    if (window.pageYOffset > 100) {
+      this.isFixed = true;
+    } else {
+      this.isFixed = false;
+    }
+  }
 }
