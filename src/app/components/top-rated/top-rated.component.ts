d0ad44b9a49ebe356055ea9c7df96a20d7b6118d
Kevin Diego <55944691+kevinDsousa@users.noreply.github.com>
2023-09-14 11:09:40 -0300

    List movies (#10)
    * feat: Create login recover and register component
    
    * wip: Create base http require
    
    * wip: cofigure request error e layout cards
    
    * feat: Create movie list, popular movier
    
    * feat: create upcoming-premiere
    
    * feat: create top-rated list

--- src/app/components/top-rated/top-rated.component.ts
+++ src/app/components/top-rated/top-rated.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MovieService } from 'src/app/service/movie.service';
+import { Movie } from 'src/app/shared/movie.model';
 
 @Component({
   selector: 'app-top-rated',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-rated.component.css']
 })
 export class TopRatedComponent {
+  moviesList: Movie[] = [];
+  currentPosition = 0;
 
+
+  constructor(private moviesService: MovieService) {
+    this.getMovies();
+  }
+
+  ngOnInit(): void {}
+
+  getMovies(): void {
+    this.moviesService.getTopRated().subscribe((movies) => {
+      this.moviesList = movies.results
+    });
+  }
+
+  handleLeft(): void {
+    if (this.currentPosition > 0) {
+      this.currentPosition--;
+      this.scrollMovieList();
+    }
+  }
+
+  handleRight(): void {
+    if (this.currentPosition < this.moviesList.length - 1) {
+      this.currentPosition++;
+      this.scrollMovieList();
+    }
+  }
+
+  scrollMovieList(): void {
+    const movieWidth = 90;
+    const translateX = -this.currentPosition * movieWidth;
+    const movieList = document.querySelector('.movie-rated-list') as HTMLElement;
+    movieList.style.transform = `translateX(${translateX}px)`;
+  }
 }
