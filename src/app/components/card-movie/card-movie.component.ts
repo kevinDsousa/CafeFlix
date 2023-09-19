968490722edf726f8b82d05c5eac264fd59c80d7
kevindsousa <kevindiegodasilvasousa@gmail.com>
2023-09-19 10:12:50 -0300

    feat: Create card detail

--- src/app/components/card-movie/card-movie.component.ts
+++ src/app/components/card-movie/card-movie.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/service/movie.service';
 import { Movie } from 'src/app/shared/movie.model';
 import { environment } from 'src/environments/environment.development';
 
@@ -9,23 +11,27 @@ import { environment } from 'src/environments/environment.development';
 })
 export class CardMovieComponent implements OnInit {
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  public readonly URLIMAGE: string = environment.URLIMAGE
+}
+  constructor(private router: ActivatedRoute, private moviesService: MovieService) {}
 
   @Input() movie!: Movie;
 
+  public readonly URLIMAGE: string = environment.URLIMAGE
+
   public showDataCard: boolean = false;
 
-  constructor() {}
+  getMovie(id: string) {
+    this.moviesService.getDetailsMovies(id).subscribe((movie) => {
+      console.log(movie);
+    })
+  }
+
   /**
    * Mostra valores com base no hover
    * @param $event Evento de hover mostra template
    */
   showValues($event: MouseEvent): void {
     this.showDataCard = true;
-    console.log(this.showDataCard);
   }
 
   /**
@@ -34,7 +40,6 @@ export class CardMovieComponent implements OnInit {
    */
   hideValues($event: MouseEvent): void {
     this.showDataCard = false
-    console.log(this.showDataCard);
   }
 
 }
